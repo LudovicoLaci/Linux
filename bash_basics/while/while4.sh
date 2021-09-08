b7249454bf9aad083efd75cc1cf8e838b20c6edb
LudovicoLaci <ludovico.laci@gmail.com>
2021-09-08 01:14:35 +0000

    First commit

---
+++ bash_basics/while/while4.sh
@@ -0,0 +1,28 @@
+###################################################################
+#	Script Name: while4	                                  #                                                           
+#	Description: Creating infinite loop	                  #                                                     
+#	Author: LudLac						  #
+###################################################################
+
+#!/bin/bash
+
+n=1
+while :
+do
+         printf "The current value of n=$n\n"
+         if [ $n == 3 ]
+         then
+                   echo "good"
+         elif [ $n == 5 ]
+         then
+                  echo "bad"
+         elif [ $n == 7 ]
+         then
+                  echo "ugly"
+         elif [ $n == 10 ]
+         then
+                   exit 0
+         fi
+         ((n++))
+done
+
